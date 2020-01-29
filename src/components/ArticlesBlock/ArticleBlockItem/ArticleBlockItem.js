99f4ea071eb308f436c5fae50120b7ad648ca652
Neil Dewing <neildewing@Neils-MacBook-Pro.local>
2020-01-29 16:57:19 +0000

    fiddling with images - annyingly

--- src/components/ArticlesBlock/ArticleBlockItem/ArticleBlockItem.js
+++ src/components/ArticlesBlock/ArticleBlockItem/ArticleBlockItem.js
@@ -1,15 +1,34 @@
 import React from "react"
-
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 import styles from "./ArticleBlockItem.module.scss"
 
-export default props => (
-  <div className={styles.articleItem}>
-    <img
-      src={props.articleContent.img}
-      alt={props.articleContent.title}
-      className={styles.articleItemImg}
-    />
-    <h2 className={styles.articleItemTitle}>{props.articleContent.title}</h2>
-    <p className={styles.articleItemBody}>{props.articleContent.body}</p>
-  </div>
-)
+export default props => {
+  //const imgPath = props.articleContent.img
+  const data = useStaticQuery(
+    graphql`
+      query getImage($imgPath: String) {
+        imageOne: file(relativePath: { eq: $imgPath }) {
+          childImageSharp {
+            fluid(maxWidth: 1000) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `,
+    { fileName: props.articleContent.img }
+  )
+
+  return (
+    <div className={styles.articleItem}>
+      <Img
+        fluid={data.imageOne.childImageSharp.fluid}
+        alt={props.articleContent.title}
+        className={styles.articleItemImg}
+      />
+      <h2 className={styles.articleItemTitle}>{props.articleContent.title}</h2>
+      <p className={styles.articleItemBody}>{props.articleContent.body}</p>
+    </div>
+  )
+}
