286003db5e730ef1c8ddeb45a18c93326b727ac6
Neil Dewing <neildewing@Neils-MacBook-Pro.local>
2020-02-10 10:26:41 +0000

    .

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Link } from "gatsby"
+import ToTop from "./ToTop/ToTop"
+import styles from "./Footer.module.scss"
+
+export default () => (
+  <footer className={styles.siteFooter}>
+    <ToTop />
+    <div className="container">
+      <p>
+        ©2019 Rooster Design <br />
+        <Link to="/privacy" className={styles.siteFooterLink}>
+          Privacy &amp; Legal
+        </Link>
+      </p>
+      <p>
+        Rooster Design is registered in England and Wales, No: 07335844, VAT No:
+        996320685
+      </p>
+      <p>Registered Address: 7 Dale Close, Long Itchington, CV47 9SE</p>
+    </div>
+  </footer>
+)
